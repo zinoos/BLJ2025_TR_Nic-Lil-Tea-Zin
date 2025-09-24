70ec7375643bbe053c8b7ddfd6d7e67211bc801f
ny-nico-s <nico.schult@noseryoung.ch>
2025-09-24 13:26:03 +0200

    multiplikation und fukulität

--- calculate_results.h
+++ calculate_results.h
@@ -7,5 +7,9 @@
 
 void user_input_sum();
 int calculate_sum(int a, int b);
+void user_input_faculty();
+int calculate_faculty(int n);
+void user_input_multiply();
+int calculate_multiply(int a, int b);
 
 #endif //C_CALCULATOR_CALCULATE_RESULTS_H
